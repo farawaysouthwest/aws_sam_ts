f68f13c3d9065b38b889989686e5c3792b05eb44
Mitchell Alderson <mitch@farawaysouthwest.com>
2021-10-18 22:14:39 -0700

    setup test framework

---
+++ function/tests/unit/test-handler.ts
@@ -0,0 +1,18 @@
+import { lambdaHandler } from "../../app";
+import { expect } from "chai";
+import { APIGatewayProxyResultV2 } from "aws-lambda";
+let event: any, context: any;
+
+describe("Tests index", function () {
+  it("verifies successful response", async () => {
+    const result = (await lambdaHandler(event, context)) as any;
+
+    expect(result).to.be.an("Object");
+    expect(result.statusCode).to.equal(200);
+    expect(result.body).to.be.an("string");
+
+    let response = JSON.parse(result.body);
+
+    expect(response).to.be.an("string");
+  });
+});
